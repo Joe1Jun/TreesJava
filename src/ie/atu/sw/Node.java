d86bf11dbe08a1b03a5d2c6c3c400d89da90f7aa
Joe1Jun <junker91joe@gmail.com>
2024-10-02 17:38:19 +0100

    Initial Commit

---
+++ src/ie/atu/sw/Node.java
@@ -0,0 +1,26 @@
+package ie.atu.sw;
+
+import java.util.*;
+
+public interface Node <T> {
+	//The T thats used here is a generic which means that 
+	// it it a placeholder for the datatypes that wil be inserted by the program later
+	// by the concrete classes that will use the methods from the interface
+	
+	public T getData();
+	public void setData( T data);
+	
+	public boolean isRoot();
+	public boolean isLeaf();
+	public Node<T> getParent();
+	public List<Node<T>> children();
+	
+	public void addChild(Node<T> child);
+	public void removeChild(Node <T> child);
+	public void hasChild(Node <T> child);
+	public boolean hasSibling();
+	public List <Node <T>> siblings();
+	
+	
+
+}
---
+++ src/ie/atu/sw/Runner.java
@@ -0,0 +1,10 @@
+package ie.atu.sw;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		
+		Node<String> node = null;
+	}
+
+}
